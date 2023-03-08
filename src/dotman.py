89b1d6a36353ca1d35e09343f2baa398c7fb3abf
Emmett de St. Croix <emmett@edestcroix.ca>
2023-03-08 10:55:11 -0800

    add git controls, improve argument parsing

--- src/dotman.py
+++ src/dotman.py
@@ -5,6 +5,15 @@ import argparse
 import shutil as sh
 import json
 import subprocess as sp
+from enum import Enum
+
+class Git(Enum):
+    ADD = 0
+    COMMIT = 1
+    PUSH = 2
+    STATUS = 3
+    RESTORE = 4
+    CUSTOM = 5
 
 CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".config", "dotman", "config.json")
 
@@ -74,13 +83,12 @@ def deploy_file(store_dir, dotfile):
             sh.copy(store_path, deploy_path)
         print(f"Deployed {store_path} to {deploy_path}")
 
-def deploy(store_dir, dotfiles: tuple, single_file=None):
-    if single_file:
-        dotfile = dotfiles[single_file]
+def deploy(store_dir, dotfiles: tuple, ignored=()):
+    for dotfile in dotfiles:
+        if dotfile['store_name'] in ignored:
+            print(f"Skipping {dotfile['store_name']}")
+            continue
         deploy_file(store_dir, dotfile)
-    else:
-        for dotfile in dotfiles:
-            deploy_file(store_dir, dotfile)
 
 
 def retreive_file(store_dir, dotfile):
@@ -104,19 +112,22 @@ def retreive_file(store_dir, dotfile):
         print(f"Retreived {temp_souce} as {temp_store}")
 
 
-def retreive(store_dir, dotfiles: tuple, single_file=None):
+def retreive(store_dir, dotfiles: tuple, ignored=()):
     store_dir = os.path.expanduser(store_dir)
-    if single_file:
-        retreive_file(store_dir, dotfiles[single_file])
-    else:
-        for dotfile in dotfiles:
-            retreive_file(store_dir, dotfile)
+    for dotfile in dotfiles:
+        if dotfile['store_name'] in ignored:
+            print(f"Skipping {dotfile['store_name']}")
+            continue
+        retreive_file(store_dir, dotfile)
 
 
-def diff(store_dir, dotfiles):
+def diff(store_dir, dotfiles, ignored=()):
     store_dir = os.path.expanduser(store_dir)
     are_diffs = False
     for dotfile in dotfiles:
+        if dotfile['store_name'] in ignored:
+            print(f"Skipping {dotfile['store_name']}")
+            continue
         store_path = f"{store_dir}/{dotfile['store_name']}"
         source_path = os.path.expanduser(dotfile['source_path'])
         if not os.path.exists(store_path):
@@ -163,10 +174,9 @@ def clean(store_dir, dotfiles, ignored):
     cleaned = 0
     is_dir  = False
     for file in files:
-        file, is_dir = (f"{file}/", True) if os.path.isdir(file) else (file, False)
         # check if file is in a store_name value in dotfiles
-        if file == ".git/" or file in ignored: 
-            print(f"Skipping {file}")
+        if file == (".git") or file in ignored: 
+            print(f"Skipping {'directory ' if os.path.isdir(file) else 'file '}{file}")
             continue
         if (
             file not in [dotfile['store_name'] for dotfile in dotfiles.values()]
@@ -177,41 +187,122 @@ def clean(store_dir, dotfiles, ignored):
             cleaned += 1
     print(f"Cleaned up {cleaned} unmanaged files" if cleaned else "Nothing to do")
         
+
+def get_args():
+
+    parser = argparse.ArgumentParser(description="Dotman is a tool for managing dotfiles")
+    parser.add_argument("-c", "--config", help="Specify config file", action="store")
+    parser.add_argument("-C", "--clean", help="Remove unmanaged dotfiles", action="store_true")
+    parser.add_argument("-l", "--list", help="List managed dotfiles", action="store_true")
+    subparsers = parser.add_subparsers(dest="subparser_name")
+
+    deploy_parser = subparsers.add_parser("deploy")
+    deploy_parser.add_argument("-a", "--all", help="Deploy all dotfiles", action="store_true")
+    deploy_parser.add_argument("-f", "--file", help="Deploy a specific dotfile", action="store")
+    deploy_parser.add_argument("-i", "--ignore", help="Ignore a specific dotfile", action="store")
+
+    retreive_parser = subparsers.add_parser("retreive")
+    retreive_parser.add_argument("-a", "--all", help="Retreive all dotfiles", action="store_true")
+    retreive_parser.add_argument("-f", "--file", help="Retreive a specific dotfile", action="store")
+    retreive_parser.add_argument("-i", "--ignore", help="Ignore a specific dotfile", action="store")
+
+    diff_parser = subparsers.add_parser("diff")
+    diff_parser.add_argument("-a", "--all", help="Diff all dotfiles", action="store_true")
+    diff_parser.add_argument("-f", "--file", help="Diff a specific dotfile", action="store")
+    diff_parser.add_argument("-i", "--ignore", help="Ignore a specific dotfile", action="store")
+
+
+    git_parser = subparsers.add_parser("git")
+    git_parser.add_argument("-a", "--add", help="Add dotfiles to dotfile repo", action="store", default="")
+    git_parser.add_argument('-r', "--restore", help="restore staged dotfiles", action="store", default='')
+    git_parser.add_argument("-c", "--commit", help="commit dotfiles to dotfile repo", action="store", type=str)
+    git_parser.add_argument("-p", "--push", help="push dotfiles to dotfile repo", action="store_true")
+    git_parser.add_argument("-s", "--status", help="show git status of dotfile repo", action="store_true")
+    git_parser.add_argument("-C", "--command", help="run specific git command", action="store")
+    # add diff argument
+    git_parser.add_argument("-d", "--diff", help="show git diff of dotfile repo", action="store_true")
+
+    return parser.parse_args()
+
+
+def git(store_dir, action, commit_msg='dotman commit', override_cmd='', add='.'):
+    git_path = os.path.expanduser(f"{store_dir}")
+    if action == Git.ADD:
+        sp.run(["git", "-C", git_path, "add", *add.split(' ')])
+    if action == Git.COMMIT:
+        if sp.run(["git", "-C", git_path, "diff-index", "--quiet", "HEAD", "--"]).returncode == 0:
+            print("No changes to commit")
+        else:
+            sp.run(["git", "-C", git_path, "commit", "-m", commit_msg]) 
+
+    if action == Git.PUSH:
+        output = sp.run(["git", "-C", git_path, "push"], capture_output=True)
+        check = output.stderr.decode('utf-8')
+        if "Permission denied (publickey)" in check:
+            if ssh_key := input(
+                "SSH key not found, please enter path to ssh key: "
+            ):
+                sp.run(["ssh-add", os.path.expanduser(ssh_key)])
+            else:
+                print("No SSH key provided, exiting")
+            sp.run(["git", "-C", git_path, "push"])
+        else:
+            print(check)
+
+    if action == Git.STATUS:
+        sp.run(["git", "-C", git_path, "status"])
+
+    if action == Git.CUSTOM:
+        git = sp.run(["git", "-C", git_path, *override_cmd.split(' ')])
+        if git.stdout:
+            print(git.stdout)
+
+
+def git_action(config, args):
+    # add, commit, and push can be specified together,
+    # all others are mutually exclusive
+    if args.add and args.commit:
+        git(config['store_dir'], Git.ADD, add=args.add)
+        git(config['store_dir'], Git.COMMIT, commit_msg=args.commit)
+        if args.push:
+            git(config['store_dir'], Git.PUSH)
+
+    elif args.commit:
+        git(config['store_dir'], Git.COMMIT, commit_msg=args.commit)
+    elif args.status:
+        git(config['store_dir'], Git.STATUS)
+    elif args.push:
+        git(config['store_dir'], Git.PUSH)
+    elif args.command:
+        git(config['store_dir'], Git.CUSTOM, override_cmd=args.command)
+    elif args.diff:
+        git(config['store_dir'], Git.CUSTOM, override_cmd='diff')
+    elif args.add:
+        git(config['store_dir'], Git.ADD, add=args.add)
+    elif args.restore:
+        git(config['store_dir'], Git.CUSTOM, override_cmd=f'restore --staged {args.restore}')
             
 def main():
-    parser = argparse.ArgumentParser(description="Dotman is a tool for managing dotfiles")
-    parser.add_argument("-d", "--deploy", help="Deploy dotfiles", default="")
-    parser.add_argument("-r", "--retreive", help="Retreive dotfiles from repo", default="")
-    parser.add_argument("-c", "--config", help="Specify config file", action="store")
-    parser.add_argument("-D", "--diff", help="View diff between deployed and stored dotfiles", action="store_true")
-    parser.add_argument("-l", "--list", help="List all stored dotfiles", action="store_true")
-    parser.add_argument("-C", "--clean", help="Remove unmanaged dotfiles", action="store_true")
-    args = parser.parse_args()
-
-
-    if args.deploy and args.retreive:
-        print("Cannot deploy and retreive at the same time")
-        exit(1)
+    args = get_args()
 
     config = ConfigDict(args.config or CONFIG_PATH)
-    # already checked that deploy and retreive are not both set,
-    # so only one of them can have a specified file
-    dotfile = (args.deploy or args.retreive) or "all"
-    # if dotfile is all operate on all dotfiles, otherwise only the one specified
-    files = tuple(config["dotfiles"].values()) if dotfile == "all" else (config[f'dotfiles.{dotfile}'], )
-    if args.deploy:
-        deploy(config["store_dir"], files)
-    elif args.retreive:
-        retreive(config["store_dir"], files)
 
-    if args.diff:
-        diff(config["store_dir"], config["dotfiles"])
+    select_files = lambda x: tuple(config['dotfiles'].values()) if x else (config[f'dotfiles.{args.file}'], )
 
-    if args.list:
-        list(config["store_dir"], config["dotfiles"])
+    if args.subparser_name is None:
+        if args.clean: 
+            clean(config['store_dir'], config["dotfiles"], config['ignored'])
+        elif args.list:
+            list(config['store_dir'], config["dotfiles"])
+    elif args.subparser_name == "git":
+        git_action(config, args)
 
-    if args.clean:
-        clean(config["store_dir"], config["dotfiles"], config['ignored_files'])
+    else:
+        actions = {'deploy': deploy, 'retreive': retreive, 'diff': diff}
+        files = select_files (args.all or (args.file is None))
+        ignored = args.ignore.split(',') if args.ignore else []
+        actions[args.subparser_name](config['store_dir'], files, ignored)
+
 
 if __name__ == "__main__":
     main()
