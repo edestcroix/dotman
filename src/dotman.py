bf6a5f4e9b9922e21cfb923f33411de93a640abb
Emmett de St. Croix <emmett@edestcroix.ca>
2023-03-20 10:38:35 -0700

    refactor diff to be more simple again

--- src/dotman.py
+++ src/dotman.py
@@ -55,68 +55,15 @@ class ConfigDict:
             flat_dotfiles |= dotfiles[catagory]
         return flat_dotfiles
 
-
-# NOTE: This might be better if instead of diffing a backup,
-# just check if the stored file is different from the
-# one in the git repo.
-
-
-# Performs a three-way diff between the source, deploy,
-# and backup files. Returns 0 if the store and deploy
-# are the same, 1 if the store and backup are the same,
-# 2 if both the store and backup are different from the deploy,
-# and -1 if only the store and backup are different from the deploy
-def diff_status(store_dir, store, deploy):
+# diff the stored file with the deploy file
+# if returns the result of the diff if there is a diff
+# None otherwise
+def diff_status(store, deploy):
     diff = sp.run(["diff", "-bur", store, deploy], capture_output=True)
-    output = diff.stdout.decode("utf-8")
-    # the output contains lines starting with ---
-    # and +++. These will be sequential. For every
-    # --- line, add it to a pair with the +++ line
-    # that follows it and store in list
-    output = output.splitlines()
-    diff_pairs = [
-        (line, output[i + 1])
-        for i, line in enumerate(output)
-        if line.startswith("---")
-    ] 
-    # for each diff pair,
-    # parse the timestamps from the strings
-    if not diff_pairs:
-        return 0
-    pair = diff_pairs[0]
-    # at this point, the diff is between the stored file and the
-    # deployed file. If the stored file differs from the
-    # backup file, return True, because both files have
-    # changed since the last retrieval
+    return diff.stdout.decode("utf-8")
 
-
-
-    dir_one_file = pair[0].split()[1]
-    dir_two_file = pair[1].split()[1]
-
-    # new idea: at this point, we know that 
-    # dir_one_file and dir_two_file are different
-    # so check the git repo in store_dir.
-    # if dir_one_file is in sync with the git repo,
-    # then return 1, since only the deployed file is changed.
-    # if dir_one_file is not in sync with 
-
-    backup = dir_one_file.replace(store_dir, BACKUP_PATH)
-
-    backup_diff_one = sp.run(["diff", "-q", dir_one_file, backup], capture_output=True).returncode
-    backup_diff_two = sp.run(["diff", "-q", dir_two_file, backup], capture_output=True).returncode
-
-    print(backup_diff_one, backup_diff_two)
-        
-    # return -1 if only backup_diff_one is one 
-    if backup_diff_one == 1 and backup_diff_two == 0:
-        return -1
-    if backup_diff_one == 0 and backup_diff_two == 1:
-        return 1
-    return backup_diff_one + backup_diff_two
-
-def confirm_overwrite(store_dir, dir_one, dir_two):
-    if os.path.exists(dir_one) and os.path.exists(dir_two) and diff_status(store_dir, dir_one, dir_two):
+def confirm_overwrite(dir_one, dir_two):
+    if os.path.exists(dir_one) and os.path.exists(dir_two) and diff_status(dir_one, dir_two):
         print(f"{collapse_user(dir_one)} has been modified since {collapse_user(dir_two)} was stored")
         if input("Overwrite? [y/N] ").lower() != "y":
             print(f"Skipping {collapse_user(dir_one)}")
@@ -155,11 +102,15 @@ def longest_dir_len(dirs):
 
 
 # since the only difference between deploy and retreive is the
-# direction of the copy, we can use the same functions for both
+# direction of the copy, we can use the same functions for both.
 # this function prepares the paths to copy, if outgoing is true,
 # it swaps the positions of paths in the return list
 # that is, if outgoing is true, the paths will be copied from
 # the store to the deploy locations, and vice-versa if false
+# additionally, when outgoing, asks for comfirmation before copying if
+# the files have been modified since they were last stored
+# (assumed use of dotman is that the store-files shouldn't be edited,
+#   so if the files are different, assume it's the deployed one)
 def prepare_copies(store_dir: str, dotfiles: dict, ignored: list, outgoing: bool, backup=None):
     paths_to_copy = []
     for catagory, cur_dotfiles in dotfiles.items():
@@ -182,7 +133,7 @@ def prepare_copies(store_dir: str, dotfiles: dict, ignored: list, outgoing: bool
                 # confirm overwrite if both files exist and the deployment
                 # file has been modified since the last time the store file
                 # was updated
-                if outgoing and confirm_overwrite(store_dir, deploy_path, store_path):
+                if outgoing and confirm_overwrite(deploy_path, store_path):
                     paths_to_copy.append(
                         (store_path, deploy_path)
                     )
@@ -201,21 +152,9 @@ def deploy(store_dir: str, dotfiles: dict, ignored=()):
 
 def retreive(store_dir: str, dotfiles: dict, ignored=()):
     paths_to_copy = prepare_copies(store_dir, dotfiles, ignored, False)
-    sh.rmtree(BACKUP_PATH)
-    os.makedirs(BACKUP_PATH, exist_ok=True)
-    backup_copies = prepare_copies(store_dir, dotfiles, ignored, True, BACKUP_PATH)
     pad_len = longest_dir_len(paths_to_copy)
-
     for retreive_path, store_path in paths_to_copy:
         copy_file(retreive_path, store_path, pad_len)
-
-    # also copy the just-retrieve files into the
-    # backup directory. This is so that the backup
-    # is always up to date so three-way diffs
-    # can be performed.
-    for store_path, deploy_path in backup_copies:
-        copy_file(store_path, deploy_path, pad_len, True)
-
 
 
 def diff(store_dir, dotfiles, ignored=()):
@@ -262,11 +201,10 @@ def list_dotfiles(store_dir, dotfiles, flat_dotfiles):
             d_name = f"{name}/" if os.path.isdir(store_path) else name
             deploy_path = os.path.expanduser(path)
             is_stored = os.path.exists(store_path)
-            state = diff_status(store_dir, store_path, deploy_path)
+            state = 1 if diff_status(store_path, deploy_path) else 0
             print_s(f"  {name:{name_width}} ")
             print_s("[s" if is_stored else "[ ")
-            print_s("<" if state >= 1 else "")
-            print_s(">" if state in (-1, 2) else "")
+            print_s("<" if state else "")
             print_s(f"{']':{dir_buf - state}}")
             print_s(f" {short_store_dir}/{catagory}/{d_name}")
             print(f" -> {re.sub(r'^.*/home/[^/]+', '~', deploy_path)}")
